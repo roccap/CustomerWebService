da13305e3492d928688a7169b1eb2322bef23deb
Paul Rocca <Paul@the-rocca-family.net>
2018-05-10 22:51:48 +0100

    removed function

--- src/com/db/customer/AddNewCustomer.java
+++ src/com/db/customer/AddNewCustomer.java
@@ -20,15 +20,4 @@ public class AddNewCustomer {
 			return -1;
 	}
 	 
-	@Path("{c}")
-	@GET
-	@Produces("application/xml")
-	public String convertCtoFfromInput(@PathParam("c") Double c) {
-		Double fahrenheit;
-		Double celsius = c;
-		fahrenheit = ((celsius * 9) / 5) + 32;
- 
-		String result = "@Produces(\"application/xml\") Output: \n\nC to F Converter Output: \n\n" + fahrenheit;
-		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
-	}
 }
