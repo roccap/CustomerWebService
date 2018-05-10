e6cc9fb3561d89622efd8ee9e7396d93ab495780
Paul Rocca <Paul@the-rocca-family.net>
2018-05-10 23:48:59 +0100

    messing with newCustomer

--- src/com/db/customer/AddNewCustomer.java
+++ src/com/db/customer/AddNewCustomer.java
@@ -1,6 +1,8 @@
 package com.db.customer;
 
 
+import java.io.IOException;
+
 /**
  * @author roccap
  */
@@ -9,15 +11,54 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
- 
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Path("/addNewCustomer")
 public class AddNewCustomer {
 
-	@Path("/addNewCustomer")
+
 	@GET
 	@Produces("application/xml")
 	public int addCustomer() {		
 	 
 			return -1;
 	}
+	
+	@Path("{s}")
+	@GET
+	@Produces("application/json")
+	public Response addCustomer(@PathParam("s") String customerData) throws JSONException {
+
+		ObjectMapper mapper = new ObjectMapper();
+		String name = "ziggy";
+        try {
+            Customer customer = mapper.readValue(customerData, Customer.class);
+            
+            name = customer.getFirstName();
+            System.out.print(name);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		JSONObject jsonObject = new JSONObject();
+	/*	float celsius;
+		celsius =  (f - 32)*5/9; 
+		jsonObject.put("F Value", f); */
+		jsonObject.put("Success", name);
+
+		String result = "@Produces(\"application/json\") Output: \n\nAdd Customer: \n\n" + jsonObject;
+		return Response.status(200).entity(result).build();
+	  }
 	 
 }
+
+/* 
+ {firstName:"Bob",lastName:"Smith"} 
+  
+  
+ * */
